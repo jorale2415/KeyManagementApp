72c5b50214169f805700bf3437cc6cdf5d9a4888
jorale2415 <60239297+jorale2415@users.noreply.github.com>
2021-07-11 15:32:28 -0500

    Add property update

---
+++ KeyManagementApp/Models/DALProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace KeyManagementApp.Models
+{
+    public class DALProperties : BindableObject
+    {
+        public void AddProperty(Properties propertyToAdd)
+        {
+
+        }
+    }
+}
--- KeyManagementApp/ViewModels/AddPropertyViewModel.cs
+++ KeyManagementApp/ViewModels/AddPropertyViewModel.cs
@@ -1,14 +1,83 @@
-﻿using System;
+﻿using KeyManagementApp.Models;
+using KeyManagementApp.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace KeyManagementApp.ViewModels
 {
-    class AddPropertyViewModel
+    class AddPropertyViewModel : BindableObject
     {
+        public ICommand AddProperty { get; }
+
         public AddPropertyViewModel()
         {
+            AddProperty = new Command(OnAddProperty);
+        }
 
+        public string address = "";
+        public string city = "";
+        public string state = "";
+        public string zip = "";
+
+        public string Address 
+        { 
+            get => address;
+            set
+            {
+                if (value == address)
+                    return;
+
+                address = value;
+                OnPropertyChanged();
+            }
+        }
+        public string City
+        {
+            get => city;
+            set
+            {
+                if (value == city)
+                    return;
+
+                city = value;
+                OnPropertyChanged();
+            }
+        }
+        public string State
+        {
+            get => state;
+            set
+            {
+                if (value == state)
+                    return;
+
+                state = value;
+                OnPropertyChanged();
+            }
+        }  
+        public string Zip
+        {
+            get => zip;
+            set
+            {
+                if (value == zip)
+                    return;
+
+                zip = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void OnAddProperty()
+        {
+            // make sure all fields are filled out
+            // create property
+            // add variables to property
+            // open connection to db
+            // pass object to db
         }
     }
 }
